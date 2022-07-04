f00d9d2bcf03319cee20870897de0e222923be94
Jau2002 <idamendjaume@gmail.com>
2022-07-04 15:01:21 -0500

    perf(data): async await
    Restructure the fetch request from promises to an asynchronous function.

--- src/services/data.js
+++ src/services/data.js
@@ -1,43 +1,40 @@
 import axios from 'axios';
 
 async function data(city, setCities) {
-	const API_KEY = process.env.REACT_APP_API_KEY;
-	await axios
-		.get(
+	try {
+		const API_KEY = process.env.REACT_APP_API_KEY;
+		const resolve = await axios.get(
 			`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-		)
-		.then(res => {
-			if (res.data) {
-				const weatherData = {
-					longitud: res.data.coord.lon,
-					latitud: res.data.coord.lat,
-					imgMeteorology: res.data.weather[0].icon,
-					description: res.data.weather[0].description,
-					temp: res.data.main.temp,
-					temperatureMinimum: Math.round(res.data.main.temp_min),
-					temperatureMaximum: Math.round(res.data.main.temp_max),
-					pressureAtmosphere: res.data.main.pressure,
-					humidity: res.data.main.humidity,
-					windSpeed: res.data.wind.speed,
-					windDegrees: res.data.wind.deg,
-					abbreviation: res.data.sys.country,
-					timeZone: res.data.timezone,
-					id: res.data.id,
-					stateRecognized: res.data.name
-				};
-				setCities(prevCities =>
-					prevCities.some(
-						c => c.stateRecognized === weatherData.stateRecognized
-					)
-						? prevCities
-						: [weatherData, ...prevCities]
-				);
-			}
-		})
-		.catch(err => {
-			const message = err.bold();
-			throw new Error(`Your search ${message} did not match any location.`);
-		});
+		);
+		const response = await resolve.data;
+		if (response) {
+			const weatherData = {
+				longitud: response.coord.lon,
+				latitud: response.coord.lat,
+				imgMeteorology: response.weather[0].icon,
+				description: response.weather[0].description,
+				temp: response.main.temp,
+				temperatureMinimum: Math.round(response.main.temp_min),
+				temperatureMaximum: Math.round(response.main.temp_max),
+				pressureAtmosphere: response.main.pressure,
+				humidity: response.main.humidity,
+				windSpeed: response.wind.speed,
+				windDegrees: response.wind.deg,
+				abbreviation: response.sys.country,
+				timeZone: response.timezone,
+				id: response.id,
+				stateRecognized: response.name
+			};
+			setCities(prevCities =>
+				prevCities.some(c => c.stateRecognized === weatherData.stateRecognized)
+					? prevCities
+					: [weatherData, ...prevCities]
+			);
+		}
+	} catch (error) {
+		const message = error.bold();
+		throw new Error(`Your search ${message} did not match any location.`);
+	}
 }
 
 export default data;
